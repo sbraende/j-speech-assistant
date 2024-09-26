410841706e0a02beede82d412a3b38906937c82f
sbraende <sebastian.braende@gmail.com>
2024-09-26 14:41:55 +0200

    Rebuildt project

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+// const buttonEL = document.getElementById("sound")
+
+// buttonEL.addEventListener("click", () => {
+//     console.log("Hello!")
+//     const sound = new Audio('../assets/sounds/ttsMP3.com_VoiceText_2024-9-25_18-57-29.mp3')
+//     sound.play()
+// })
