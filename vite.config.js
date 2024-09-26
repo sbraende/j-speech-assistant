c5d32a091eb5190c42d9abcf51b0ef45c5f0285f
sbraende <sebastian.braende@gmail.com>
2024-09-26 21:33:00 +0200

    Added proper vite pwa to project

---
+++ vite.config.js
@@ -0,0 +1,35 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [VitePWA({
+    registerType: 'autoUpdate',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'J-Speech-Assistant',
+      short_name: 'J-Speech',
+      description: 'Soundboard to help with speech',
+      theme_color: '#ffffff',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+})
