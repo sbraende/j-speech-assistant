b14c256921a796915669bbe3c023f6f59e6e0f25
sbraende <sebastian.braende@gmail.com>
2024-09-28 11:26:11 +0200

    Grid adjusted for bigger buttons, touchend event added

--- src/js/index.js
+++ src/js/index.js
@@ -31,6 +31,39 @@ document.getElementById("unnskyld").addEventListener("click", () => {
 document.getElementById("bra").addEventListener("click", () => {
     playSound("bra")
 })
+document.getElementById("ja").addEventListener("touchend", () => {
+    playSound("ja")
+})
+document.getElementById("nei").addEventListener("touchend", () => {
+    playSound("nei")
+})
+document.getElementById("vet-ikke").addEventListener("touchend", () => {
+    playSound("vet-ikke")
+})
+document.getElementById("ferdig").addEventListener("touchend", () => {
+    playSound("ferdig")
+})
+document.getElementById("torst").addEventListener("touchend", () => {
+    playSound("torst")
+})
+document.getElementById("sulten").addEventListener("touchend", () => {
+    playSound("sulten")
+})
+document.getElementById("du-gjor").addEventListener("touchend", () => {
+    playSound("du-gjor")
+})
+document.getElementById("jeg-gjor").addEventListener("touchend", () => {
+    playSound("jeg-gjor")
+})
+document.getElementById("stopp").addEventListener("touchend", () => {
+    playSound("stopp")
+})
+document.getElementById("unnskyld").addEventListener("touchend", () => {
+    playSound("unnskyld")
+})
+document.getElementById("bra").addEventListener("touchend", () => {
+    playSound("bra")
+})
 
 
 const playSound = (soundId) => {
