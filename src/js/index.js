e90fa50bbdd64dd5f8c308d4a2f1c8a419b6dd18
sbraende <sebastian.braende@gmail.com>
2024-09-28 11:47:37 +0200

    touchend only eventlistener

--- src/js/index.js
+++ src/js/index.js
@@ -1,36 +1,36 @@
-document.getElementById("ja").addEventListener("click", () => {
-    playSound("ja")
-})
-document.getElementById("nei").addEventListener("click", () => {
-    playSound("nei")
-})
-document.getElementById("vet-ikke").addEventListener("click", () => {
-    playSound("vet-ikke")
-})
-document.getElementById("ferdig").addEventListener("click", () => {
-    playSound("ferdig")
-})
-document.getElementById("torst").addEventListener("click", () => {
-    playSound("torst")
-})
-document.getElementById("sulten").addEventListener("click", () => {
-    playSound("sulten")
-})
-document.getElementById("du-gjor").addEventListener("click", () => {
-    playSound("du-gjor")
-})
-document.getElementById("jeg-gjor").addEventListener("click", () => {
-    playSound("jeg-gjor")
-})
-document.getElementById("stopp").addEventListener("click", () => {
-    playSound("stopp")
-})
-document.getElementById("unnskyld").addEventListener("click", () => {
-    playSound("unnskyld")
-})
-document.getElementById("bra").addEventListener("click", () => {
-    playSound("bra")
-})
+// document.getElementById("ja").addEventListener("click", () => {
+//     playSound("ja")
+// })
+// document.getElementById("nei").addEventListener("click", () => {
+//     playSound("nei")
+// })
+// document.getElementById("vet-ikke").addEventListener("click", () => {
+//     playSound("vet-ikke")
+// })
+// document.getElementById("ferdig").addEventListener("click", () => {
+//     playSound("ferdig")
+// })
+// document.getElementById("torst").addEventListener("click", () => {
+//     playSound("torst")
+// })
+// document.getElementById("sulten").addEventListener("click", () => {
+//     playSound("sulten")
+// })
+// document.getElementById("du-gjor").addEventListener("click", () => {
+//     playSound("du-gjor")
+// })
+// document.getElementById("jeg-gjor").addEventListener("click", () => {
+//     playSound("jeg-gjor")
+// })
+// document.getElementById("stopp").addEventListener("click", () => {
+//     playSound("stopp")
+// })
+// document.getElementById("unnskyld").addEventListener("click", () => {
+//     playSound("unnskyld")
+// })
+// document.getElementById("bra").addEventListener("click", () => {
+//     playSound("bra")
+// })
 document.getElementById("ja").addEventListener("touchend", () => {
     playSound("ja")
 })
